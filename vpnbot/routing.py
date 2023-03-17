38df60b23425e534bacf75cc95c57ea9c279aa41
Elora VPN <eloravpn@gmail.com>
2023-03-17 21:54:01 +0330

    Version 0.5.7
    Signed-off-by: Elora VPN <eloravpn@gmail.com>

--- vpnbot/routing.py
+++ vpnbot/routing.py
@@ -141,7 +141,7 @@ def get_accounts_info(update: Update, context: CallbackContext):
         except Account.DoesNotExist:
             inbound_client = get_client(appglobals.V2RAY_INBOUND_ID, client['email'])
             if inbound_client:
-                x.add_row([index, "0", inbound_client['id'], "No User", client['email'],
+                x.add_row([index+1, "0", inbound_client['id'], "No User", client['email'],
                            size(client['up']), size(client['down']), size(client['total']),
                            client['expiry_time']])
             log.error("Account does not exist with email: " + client['email'])
