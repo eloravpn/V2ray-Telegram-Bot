5640126b095185e7a9a3ffd09cbfedaf420c2114
Elora VPN <eloravpn@gmail.com>
2023-03-24 17:10:44 +0430

    Version 0.6.0
    Signed-off-by: Elora VPN <eloravpn@gmail.com>

--- vpnbot/api/index.py
+++ vpnbot/api/index.py
@@ -1,11 +1,12 @@
+import base64
 import random
 
 from flask import Flask, request
 from flask import Response
 
 from vpnbot import appglobals
-from vpnbot.v2ray.v2ray import get_v2ray_configs
-from vpnbot.v2ray.vmess2sub import Conf2v2rayN, Str2Base64
+from vpnbot.v2ray.v2ray import get_v2ray_vless_configs, get_v2ray_configs
+from vpnbot.v2ray.vmess2sub import Str2Base64, Conf2v2rayN
 
 app = Flask(__name__, template_folder='./')
 
@@ -15,25 +16,35 @@ def index():
     # opening and reading the file
     args = request.args
     num_to_select = args.get("size", default=50, type=int)  # set the number to select here.
+    pt = args.get("pt", default='VMESS', type=str)  # set the protocol type VMESS or VLESS
     uuid = args.get("uuid", type=str)
     if uuid is None:
         return Response('', mimetype='text/plain')
     ip_list = './ip.txt'
     with open(ip_list) as fh:
         fstring = fh.readlines()
-    # displaying the extracted IP addresses
-    print(appglobals.V2RAY_SNI)
-    configs = get_v2ray_configs(fstring, appglobals.V2RAY_PORT, uuid,
+
+    if pt == 'VMESS':
+        configs = get_v2ray_configs(fstring, appglobals.V2RAY_PORT, uuid,
                                 appglobals.V2RAY_SNI, appglobals.V2RAY_PATH,
                                 appglobals.V2RAY_REMARK)
+    else:
+        if pt == 'VLESS':
+            configs = get_v2ray_vless_configs(fstring, 'ws', appglobals.V2RAY_PORT, uuid,
+                                          appglobals.V2RAY_SNI, appglobals.V2RAY_PATH,
+                                          appglobals.V2RAY_REMARK)
 
     list_of_random_items = random.sample(configs, num_to_select)
 
     configs_v2rayn = Conf2v2rayN(list_of_random_items)
     # configs_v2rayn = Conf2sr(configs)
 
-    text = '\n'.join(configs_v2rayn)
-    html = Str2Base64(text)
+    if pt == 'VMESS':
+        text = '\n'.join(configs_v2rayn)
+        html = Str2Base64(text)
+    else:
+        text = '\n'.join(list_of_random_items)
+        html = base64.b64encode(text.encode('utf-8'))
 
     # return Response(json.dumps(configs_v2rayn, default=vars), mimetype='application/json')
     return Response(html, mimetype='text/plain')
--- vpnbot/v2ray/v2ray.py
+++ vpnbot/v2ray/v2ray.py
@@ -1,4 +1,5 @@
 import re
+import urllib.parse
 
 ip_list = './ip.txt'
 
@@ -28,4 +29,38 @@ def get_v2ray_configs(fstring, port, uuid, host, path, remark):
             "tls": "tls"
         }
         lst.append(v2rayn_template)
+    return lst
+
+
+def get_v2ray_vless_configs(fstring, type, port, uuid, host, path, remark):
+    # declaring the regex pattern for IP addresses
+    pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    alpn = 'h2,http/1.1'
+
+    # initializing the list object
+    lst = []
+
+    for line in fstring:
+
+        if pattern.search(line) is None:
+            continue
+        ip = pattern.search(line)[0]
+        prefixTxt = '%s@%s:%s' % (uuid, ip, port)
+
+        prefix = 'vless://' + prefixTxt
+        postfixList = []
+
+        # print(urllib.parse.quote(path.encode('utf8')))
+        postfixList.append('path=%s' % urllib.parse.quote(path.encode('utf8')))
+        postfixList.append('security=%s' % 'tls')
+        postfixList.append('encryption=%s' % 'none')
+        postfixList.append('host=%s' % host)
+        postfixList.append('fp=%s' % 'chrome')
+        postfixList.append('type=%s' % type)
+        postfixList.append('sni=%s' % host)
+        # postfixList.append('alpn=%s' % urllib.parse.quote(alpn.encode('utf8')))
+
+        ilink = prefix + '?' + '&'.join(postfixList) + '#' + urllib.parse.quote(remark.encode('utf8'))
+
+        lst.append(ilink)
     return lst
